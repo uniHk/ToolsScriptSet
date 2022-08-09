fd62ea21c1f70352cf9c70b8ea1ace4bc717b3e8
uniHk <44690055+uniHk@users.noreply.github.com>
2022-08-09 23:51:11 +0800

    提取路径为全局变量，方便修改

--- Svn2Unity.py
+++ Svn2Unity.py
@@ -2,6 +2,9 @@
 import os
 import datetime
 
+UnityPath = r"E:\Unity\Editor\Unity.exe"
+ProjPath = r"E:\Project\\nssclient_publish\NssUnityProj"
+
 def doCMD(cmd):
     print(cmd)
     os.system(cmd)
@@ -16,6 +19,6 @@ def main():
     unityImportStartTime = datetime.datetime.now()
     print("\n\n\nunityImportStartTime = %s\n\n\n" % (unityImportStartTime))
 
-    doCMD(r"E:\Unity\Editor\Unity.exe -batchmode -projectPath E:\Project\Trunk_backup\NssUnityProj")
+    doCMD(UnityPath + " -batchmode -projectPath " + ProjPath)
 
 main()
